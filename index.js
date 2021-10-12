04d3e5f5e3fff40e165513c576268b52b2e7e018
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2021-10-12 17:04:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting = "Welcome, " + firstInitial + "!"
