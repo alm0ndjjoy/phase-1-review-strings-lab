b8e37f16ccd29c55347b769ceba1b5bfa64202dd
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2021-11-01 12:26:35 -0400

    commit tree 8aab13f9004754cad395eb02a3d7104b26033da0

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// Write your code in this file!
-const currentUser = "Grace Hopper";
+const currentUser = "Joyce Huang";
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-console.log(welcomeMessage);
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-console.log(excitedWelcomeMessage);
 
+console.log(excitedWelcomeMessage)
 
-const firstInitial = currentUser[0];
-const restOfName = currentUser.slice(1);
-const shortGreeting = "Welcome, " + firstInitial + "!"
+const shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
+
+console.log(shortGreeting)
